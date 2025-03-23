cc3b43a0aa185702b53d46bbc42d0044e18d0efc
Charlito7 <59016855+Charlito7@users.noreply.github.com>
2025-03-23 01:13:46 -0400

    update

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,13 +1,37 @@
 const { defineConfig } = require("@vue/cli-service");
-const webpack = require('webpack');
+const webpack = require("webpack");
+// Uncomment if you want to analyze bundle
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = defineConfig({
   transpileDependencies: true,
+
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
       }),
+      // Uncomment if you want to analyze bundle
+      // new BundleAnalyzerPlugin(),
     ],
-  }
+
+    optimization: {
+      splitChunks: {
+        chunks: "all", // split all types of chunks (async + initial)
+        automaticNameDelimiter: "-",
+        maxSize: 300000, // ~300 KB max per chunk
+      },
+    },
+  },
+
+  // Optional: gzip compression of output files (you need compression-webpack-plugin)
+  // chainWebpack: config => {
+  //   config.plugin("compression").use(require("compression-webpack-plugin"), [
+  //     {
+  //       test: /\.(js|css|html|svg)$/,
+  //       threshold: 10240, // compress files over 10 KB
+  //       minRatio: 0.8,
+  //     },
+  //   ]);
+  // },
 });
